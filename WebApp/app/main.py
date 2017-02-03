fe0a450842a8790aa63e6bd747b467c58fbac7a6
Albert Acuna <aacuna3@asu.edu>
2017-02-03 15:41:01 -0700

    Directory Changes

--- WebApp/app/main.py
+++ WebApp/app/main.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify
 
-from WebApp.app.csvReader import CSVReader, Search
+from csvReader import CSVReader, Search
 
+print(__name__)
 app = Flask(__name__)
-parsedData = CSVReader('static/CrimeData.csv')
-search = Search(parsedData)
+parsedData = CSVReader('static/CrimeStats.csv')
+search = Search(parsedData.dataArray)
 # parser = reqparse.RequestParser()
 # parser.add_argument('speedGreaterThan', type=int, help='Speed of vehicle greater than')
 # parser.add_argument('route_id', type=str, help='Speed of vehicle')
